eb0ef29ce88ca62d8705663b00c38c842b9082f7
H3adlock <sourav.ghosh700@hotmail.com>
2019-08-26 21:46:02 +0530

    0026: s3 service implemented

--- viewsfinder/settings/base.py
+++ viewsfinder/settings/base.py
@@ -26,7 +26,7 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'tinymce',
     'core',
-
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -120,6 +120,14 @@ TINYMCE_DEFAULT_CONFIG = {
     'statusbar': True,
 }
 
+# s3 storage configurations
+
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_FILE_STORAGE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # Allauth configurations
 AUTHENTICATION_BACKENDS = (
