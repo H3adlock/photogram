db92a0201077cc8abbefa0e9b2c11bb5e911bcc1
H3adlock <sourav.ghosh700@hotmail.com>
2019-09-01 17:02:19 +0530

    production update

--- viewsfinder/settings/production.py
+++ viewsfinder/settings/production.py
@@ -8,23 +8,12 @@ SECRET_KEY = os.environ['SECRET_KEY']
 TIME_ZONE = os.environ['TZ']
 ALLOWED_HOSTS = ['viewsfinder.herokuapp.com']
 
-# AUTH_PASSWORD_VALIDATORS = [
-#     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
-#     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
-#     {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
-#     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
-# ]
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',
-        'PORT': '',
-    }
-}
+AUTH_PASSWORD_VALIDATORS = [
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+]
 
 
 # heroku logging
