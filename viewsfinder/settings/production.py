b4a914b686e6db5cd930adc3c4a62a0cff60efa1
H3adlock <sourav.ghosh700@hotmail.com>
2019-09-03 09:38:36 +0530

    moving static to s3

--- viewsfinder/settings/production.py
+++ viewsfinder/settings/production.py
@@ -56,13 +56,14 @@ LOGGING = {
 
 # s3 storage configurations
 AWS_S3_FILE_OVERWRITE = False
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
 AWS_S3_REGION_NAME = os.environ['AWS_S3_REGION_NAME']
 
-
+STATICFILES_STORAGE = "viewsfinder.s3utils.StaticS3BotoStorage"
+DEFAULT_FILE_STORAGE = "viewsfinder.s3utils.MediaS3BotoStorage"
 # AWS_S3_FILE_STORAGE = False
 
 EMAIL_BACKEND = 'sendgrid_backend.SendgridBackend'
