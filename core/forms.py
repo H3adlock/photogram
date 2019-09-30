5462dce32e143229f8d6a14692704436ac863191
H3adlock <sourav.ghosh700@hotmail.com>
2019-09-30 13:06:16 +0530

    edit profile beta

--- core/forms.py
+++ core/forms.py
@@ -3,11 +3,22 @@ from tinymce import TinyMCE
 from .models import Post, Comment, Profile
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.utils.safestring import mark_safe
 
 
 class TinyMCEWidget(TinyMCE):
     def use_required_attribute(self, *args):
         return False
+
+
+class PictureWidget(forms.widgets.Widget):
+    def render(self, name, value, attrs=None):
+        html = Template("""<img src="$link"/>""")
+        return mark_safe(html.substitute(link=value))
+
+
+class CustomClearableFileInput(forms.ClearableFileInput):
+    template_name = 'widgets/customclearablefileinput.html'
 
 
 class PostForm(forms.ModelForm):
@@ -36,10 +47,13 @@ class DateInput(forms.DateInput):
 
 
 class ProfileForm(forms.ModelForm):
+    profile_picture = forms.ImageField(widget=CustomClearableFileInput)
+
     def __init__(self, *args, **kwargs):
         super(ProfileForm, self).__init__(*args, **kwargs)
         for visible in self.visible_fields():
-            visible.field.widget.attrs['class'] = 'form-control'
+            if visible.label != "Profile picture":
+                visible.field.widget.attrs['class'] = 'form-control'
 
     class Meta:
         model = Profile
--- core/models.py
+++ core/models.py
@@ -11,6 +11,7 @@ from django.dispatch import receiver
 
 
 class Profile(models.Model):
+    # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.OneToOneField(
         settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True)
